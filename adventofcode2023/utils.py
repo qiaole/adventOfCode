b43f75b2896b3da29372a185cb3130104526b828
lilyzhao <qiaole.zhao@ocado.com>
2023-12-27 08:02:49 +0000

    day5 and clean up

--- adventofcode2023/utils.py
+++ adventofcode2023/utils.py
@@ -1,5 +1,7 @@
 import re
 
+def find_number_in_string (stringa):
+    return [int(x) for x in re.findall(r'\d+', stringa)]
 
 def read_data (file, path = 'inputs/'):
     with open(path+file, 'r', encoding="utf-8") as f:
